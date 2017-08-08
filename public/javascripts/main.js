e030be8effd3aabb4cb54789814c9068d9d9ebf9
ashfaq <ashfaqpatwari@gmail.com>
2017-08-08 19:26:05 +0300

    added categories

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
         parallax: true,
-        speed: 600,
-        autoplay: 2500,
+        speed: 1000,
+        autoplay: 6000,
     });
 
     $(window).scroll(function () {
