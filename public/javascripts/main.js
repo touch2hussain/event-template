314f0d93c23fa2054b77a7cf260cc30653adc088
touch2hussain <touch2hussain@gmail.com>
2017-08-08 15:35:14 +0300

    more on home page

---
+++ public/javascripts/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        parallax: true,
+        speed: 600,
+    });
+});
