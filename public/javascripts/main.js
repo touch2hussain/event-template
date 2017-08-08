8f7715aa84fd900525619a1902cb6531be4eb229
touch2hussain <touch2hussain@gmail.com>
2017-08-08 17:23:49 +0300

    more work

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,5 +6,22 @@ $(document).ready(function () {
         prevButton: '.swiper-button-prev',
         parallax: true,
         speed: 600,
+        autoplay: 2500,
     });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
 });
