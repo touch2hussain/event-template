114bbda50850717adbdbce8782cd5cca26916b48
touch2hussain <touch2hussain@gmail.com>
2017-08-10 13:18:52 +0300

    Gallery added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -25,4 +25,74 @@ $(document).ready(function () {
         return false;
     });
 
+    // for light box
+    var groups = {};
+    var items = {
+        1: [{
+                src: 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Peter_%26_Paul_fortress_in_SPB_03.jpg/800px-Peter_%26_Paul_fortress_in_SPB_03.jpg',
+                title: 'Peter & Paul fortress in SPB'
+            },
+            {
+                src: 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Peter_%26_Paul_fortress_in_SPB_03.jpg/800px-Peter_%26_Paul_fortress_in_SPB_03.jpg',
+                title: 'india'
+            },
+            {
+                src: 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Peter_%26_Paul_fortress_in_SPB_03.jpg/800px-Peter_%26_Paul_fortress_in_SPB_03.jpg',
+                title: 'america'
+            },
+        ],
+        2: [{
+                src: 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Peter_%26_Paul_fortress_in_SPB_03.jpg/800px-Peter_%26_Paul_fortress_in_SPB_03.jpg',
+                title: 'Peter'
+            },
+            {
+                src: 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Peter_%26_Paul_fortress_in_SPB_03.jpg/800px-Peter_%26_Paul_fortress_in_SPB_03.jpg',
+                title: 'demo india'
+            },
+            {
+                src: 'http://upload.wikimedia.org/wikipedia/commons/thumb/6/64/Peter_%26_Paul_fortress_in_SPB_03.jpg/800px-Peter_%26_Paul_fortress_in_SPB_03.jpg',
+                title: 'demo america'
+            },
+        ]
+
+    }
+
+
+
+
+    $('.galleryItem').each(function () {
+        var id = parseInt($(this).attr('data-group'), 10);
+
+        if (!groups[id]) {
+            groups[id] = [];
+        }
+
+        groups[id].push(this);
+    });
+    $.each(groups, function (index) {
+        var val = parseInt(index) + 1;
+        console.log('val:', val)
+        $(this).magnificPopup({
+            items: items[index],
+            type: 'image',
+            closeOnContentClick: true,
+            closeBtnInside: false,
+            // Delay in milliseconds before popup is removed
+            removalDelay: 300,
+
+            // Class that is added to popup wrapper and background
+            // make it unique to apply your CSS animations just to this exact popup
+            mainClass: 'mfp-fade',
+
+            gallery: {
+                enabled: true,
+                arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+                tPrev: 'Previous', // title for left button
+                tNext: 'Next', // title for right button
+            }
+        })
+
+    });
+
 });
